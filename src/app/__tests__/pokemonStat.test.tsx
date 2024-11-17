52a7d87a22ad16ef4635177054a545d0d907be4c
Uzair Jawaid <uzair-jawaid@hotmail.com>
2024-11-17 14:48:02 +0500

    Added unit tests to dockerfile as step in CICD and added more tests

--- src/app/__tests__/pokemonStat.test.tsx
+++ src/app/__tests__/pokemonStat.test.tsx
@@ -2,9 +2,15 @@ import { render, screen } from '@testing-library/react';
 import PokemonStat from '../components/pokemonStat';
 
 describe('PokemonStat Component', () => {
-  it('renders with correct data', () => {
+  it('renders with correct height format and data', () => {
     render(<PokemonStat dataKey="Height" dataValue="10" units="'" />);
     expect(screen.getByText("Height")).toBeInTheDocument();
     expect(screen.getByText("10'")).toBeInTheDocument();
+  });
+
+  it('renders with correct weight format and data', () => {
+    render(<PokemonStat dataKey="Weight" dataValue="100" units="lbs" />);
+    expect(screen.getByText("Weight")).toBeInTheDocument();
+    expect(screen.getByText("100lbs")).toBeInTheDocument();
   });
 });
